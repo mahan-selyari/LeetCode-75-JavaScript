eeac244dfb9f5233864d71e64839d45cf2cd786f
Mahan Selyari <mahanselyari0@gmail.com>
2026-08-15 15:13:32 +0330

    Time: 2 ms (60.87%), Space: 56.7 MB (39.37%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -4,26 +4,26 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
-let count=0
+let counter=0
     if(flowerbed.length>1){
         if (flowerbed[0]===0 && flowerbed[1]===0){
             flowerbed[0]=1
-            count++
+            counter++
 }
          if (flowerbed[flowerbed.length-1]===0 && flowerbed[flowerbed.length-2]===0){
-            count++
+            counter++
             flowerbed[flowerbed.length-1]=1
     }}else if(flowerbed.length === 1 && flowerbed[0] === 0){
-        count++
+        counter++
     }
         for(let i = 1; i < flowerbed.length-1; i++){
             if (flowerbed[i]===0 && flowerbed[i-1]===0 && flowerbed[i+1]===0){
-              count++  
+              counter++  
               flowerbed[i]=1
             }
     } 
 
-    if (count>=n){
+    if (counter>=n){
         return true
     }else{
        return false
