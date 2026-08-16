7b39852073a1f5a6cee4b45d0f51bfc5af811961
Mahan Selyari <mahanselyari0@gmail.com>
2026-08-16 12:35:51 +0330

    Time: 2 ms (58.54%), Space: 55.8 MB (57.9%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let result = s.trim().split(" ").filter(s=>s!=="").reverse().join(" ")
+    
+    return result
+};
