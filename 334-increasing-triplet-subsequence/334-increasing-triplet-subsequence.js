c4cb3211fed7265d046eb70ceaa2d711efd72410
Mahan Selyari <mahanselyari0@gmail.com>
2026-08-17 14:53:43 +0330

    Add solution for Increasing Triplet Subsequence

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let first = Infinity;
+    let second = Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < first) {
+            first = nums[i];
+        } else if (nums[i] > first && nums[i] < second) {
+            second = nums[i];
+        }
+
+        if (nums[i] > second) {
+            return true;
+        }
+    }
+
+    return false;
+};
