ca8aa6b79beaddc67e766a1d9b1d96d871f39d13
Mahan Selyari <mahanselyari0@gmail.com>
2026-08-17 14:50:06 +0330

    Time: 3 ms (46.19%), Space: 75.5 MB (14.36%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -7,18 +7,14 @@ let first = Infinity;
 let second = Infinity;
 let flag = 0;
 for(let i = 0; i < nums.length; i++){
-    if (nums[i] < first && nums[i] < second){
+    if (nums[i] < first){
         first = nums[i];
     }else if(nums[i] > first && nums[i] < second){
         second = nums[i];
     }if(nums[i] > second){
-        flag = 1;
+        return true;
         break;
     }
-}
-if (flag){
-    return true;
-}else{
-    return false;
-}
+    
+    }return false;
 };
