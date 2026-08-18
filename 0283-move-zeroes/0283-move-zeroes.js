86555bd4dc0f60774f6a1296add21639aa770f38
Mahan Selyari <mahanselyari0@gmail.com>
2026-08-19 02:23:18 +0330

    Time: 4 ms (30.37%), Space: 60.3 MB (54.78%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let i = 0;
+    let left = 0;
+    let right = 1;
+    while(i < nums.length){
+        if (nums[left] === 0){
+            if(nums[right] !== 0 && right < nums.length){
+                [nums[left], nums[right]] = [nums[right], nums[left]]
+                left++;
+                right++;
+            }else{
+                right++;
+            }
+        }else{
+            left++
+            right++
+        }       i++;
+    }
+};
