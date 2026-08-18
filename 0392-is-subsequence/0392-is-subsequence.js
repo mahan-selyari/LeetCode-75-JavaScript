2ea37baf74379d6be1f710569c7b4bf19fca1750
Mahan Selyari <mahanselyari0@gmail.com>
2026-08-19 02:53:18 +0330

    Time: 0 ms (100%), Space: 54.7 MB (76.72%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,15 +4,12 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    let sLeft = 0;
-    for (let i = 0; i < t.length; i++){
-        if (s[sLeft] === t[i]){
-            sLeft++
+    let sIndex = 0;
+    for (let tIndex = 0; tIndex < t.length; tIndex++){
+        if (s[sIndex] === t[tIndex]){
+            sIndex++
         }
     }
-    if(sLeft === s.length){
-        return true;
-    }else{
-        return false;
-    }
+        return sIndex === s.length
+
 };
